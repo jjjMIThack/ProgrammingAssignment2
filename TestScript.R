7ff6ad29ab44126a5fd2756951ed3ebd27f62be7
jjjMIThack <jjjonesmit@gmail.com>
2015-12-27 15:34:47 -0500

    Updated for Assignment 2
    Assignment 2

---
+++ TestScript.R
@@ -0,0 +1,9 @@
+x <- matrix(c(2,3,2,2),2,2)
+
+y <- makeCacheMatrix(x)
+
+cacheSolve(y)
+
+cacheSolve(y)
+
+
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two function will calculate the inverse of an invertible matrix.
+## If the inverse has already been calculated then the inverse will be retrieved
 
-## Write a short comment describing this function
-
+## This function creates a matrix object and will place the inverse 
+##of the numeric matrix in the cache. 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvmatrix <- function(invmatrix) m <<- invmatrix
+  getinvmatrix <- function() m
+  list(set = set, get = get,
+       setinvmatrix = setinvmatrix,
+       getinvmatrix = getinvmatrix)
 }
 
-
-## Write a short comment describing this function
+## This function returns the inverse of the special "matrix" from 
+##function make ChacheMatrix.  If the inverse has already been calculated
+##then the inverse is retrieved from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvmatrix()
+  print(m)
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvmatrix(m)
+  m
 }
